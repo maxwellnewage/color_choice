8a11537ade7f91c139bb216cec8f2cec931ed22f
Maximiliano Burgos <maxib2007@gmail.com>
2023-02-03 12:05:39 -0300

    se armaron rutas para color y vote

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from typing import List
 from fastapi import FastAPI, status
-from sqlmodel import Session, select
-from storage.db import engine
-from storage.models import Vote
+from color.color_routes import color_router
+from vote.vote_routes import vote_router
 
 app = FastAPI()
 
-session = Session(bind=engine)
+app.include_router(color_router)
+app.include_router(vote_router)
 
 
 @app.get('/', status_code=status.HTTP_200_OK)
 async def home():
     return "hello api!"
 
+# session = Session(bind=engine)
 
-@app.get('/votes', response_model=List[Vote], status_code=status.HTTP_200_OK)
-async def get_all_votes():
-    return session.exec(select(Vote)).all()
+# @app.get('/votes', response_model=List[Vote], status_code=status.HTTP_200_OK)
+# async def get_all_votes():
+#     return session.exec(select(Vote)).all()

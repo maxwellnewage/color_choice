7cc772626482410064e802e3f12e4a977646834b
Maximiliano Burgos <maxib2007@gmail.com>
2023-04-11 11:35:05 -0300

    columnas definidas para color en el admin

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Vote, Color
 
 
-admin.site.register([Vote, Color])
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hexa')
+
+
+admin.site.register(Vote)
+admin.site.register(Color, ColorAdmin)

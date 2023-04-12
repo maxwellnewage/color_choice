06dab8d4015c5e27a3fd2724908b7df26fd03b64
Maximiliano Burgos <maxib2007@gmail.com>
2023-04-12 11:33:58 -0300

    se implementa un método de login que devuelve un token

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from .views import ColorViewSet, VoteViewSet
+from .views import ColorViewSet, VoteViewSet, user_auth
 
 
 router = routers.DefaultRouter()
@@ -9,3 +9,7 @@ router.register('colors', ColorViewSet, 'colors')
 router.register('votes', VoteViewSet, 'votes')
 
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('auth', user_auth, name='api-auth')
+]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import authenticate
 from django.shortcuts import render
 from rest_framework import status, viewsets, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Vote, Color
 from .serializers import ColorSerializer, VoteSerializer
 
@@ -12,3 +17,19 @@ class ColorViewSet(viewsets.ModelViewSet):
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
+
+
+@api_view(['POST'])
+def user_auth(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Se debe indicar usuario y contraseña'}, status=status.HTTP_204_NO_CONTENT)
+
+    user = authenticate(request, username=username, password=password)
+    if user is None:
+        return Response({'error': 'El usuario o la contraseña son incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key}, status=status.HTTP_200_OK)

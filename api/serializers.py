2c9a6b6c14a4367fc2b0daef55966c44d3278d82
Maximiliano Burgos <maxib2007@gmail.com>
2023-04-12 12:07:54 -0300

    fix fields

--- api/serializers.py
+++ api/serializers.py
@@ -5,9 +5,11 @@ from .models import Vote, Color
 class ColorSerializer(ModelSerializer):
     class Meta:
         model = Color
+        fields = '__all__'
 
 
 class VoteSerializer(ModelSerializer):
     class Meta:
         model = Vote
+        fields = '__all__'
         depth = 1
